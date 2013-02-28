6beda9e047e93c53c19d3213718b3d23883e0585
AsO <aso.asri@gmail.com>
2013-02-28 22:08:42 +0900

    Update PinnedArray.vb

--- PinnedArray.vb
+++ PinnedArray.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Runtime.InteropServices
 
 Namespace picotech
@@ -51,7 +51,7 @@ Namespace picotech
         End Operator
 
 #Region "IDisposable Support"
-        Private disposedValue As Boolean ' 重複する呼び出しを検出するには
+        Private disposedValue As Boolean
 
         ' IDisposable
         Protected Overridable Sub Dispose(disposing As Boolean)
@@ -59,24 +59,13 @@ Namespace picotech
             Me.disposedValue = True
 
             If disposing Then
-                ' TODO: マネージ状態を破棄します (マネージ オブジェクト)。
+                '
             End If
 
-            ' TODO: アンマネージ リソース (アンマネージ オブジェクト) を解放し、下の Finalize() をオーバーライドします。
-            ' TODO: 大きなフィールドを null に設定します。
             _pinnedHandle.Free()
         End Sub
 
-        ' TODO: 上の Dispose(ByVal disposing As Boolean) にアンマネージ リソースを解放するコードがある場合にのみ、Finalize() をオーバーライドします。
-        'Protected Overrides Sub Finalize()
-        '    ' このコードを変更しないでください。クリーンアップ コードを上の Dispose(ByVal disposing As Boolean) に記述します。
-        '    Dispose(False)
-        '    MyBase.Finalize()
-        'End Sub
-
-        ' このコードは、破棄可能なパターンを正しく実装できるように Visual Basic によって追加されました。
         Public Sub Dispose() Implements IDisposable.Dispose
-            ' このコードを変更しないでください。クリーンアップ コードを上の Dispose(ByVal disposing As Boolean) に記述します。
             Dispose(True)
             GC.SuppressFinalize(Me)
         End Sub
